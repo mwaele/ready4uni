11e9d2df7bd3465bd5568c1fb9af5cdace0ea2d8
mercy masaa <masaakavulanya@gmail.com>
2025-04-03 04:43:25 +0300

    Set up FAQ component

---
+++ frontend/src/data.jsx
@@ -0,0 +1,44 @@
+export const faqData = [
+  {
+    question: "How do I know which universities I can apply to?",
+    answer:
+      "Based on your cluster points, our tool will show you the universities that offer the courses you're eligible for. This helps you explore the best-fit options for your academic and career goals.",
+  },
+  {
+    question: "What are KUCCPS cluster points?",
+    answer:
+      "Cluster points are the weighted scores based on your high school performance, which determine your eligibility for various university courses through KUCCPS. These points help match you to the most suitable courses.",
+  },
+  {
+    question: "What is the formula used to calculate cluster points?",
+    answer:
+      "We use a specific formula that considers the grades in certain subjects (e.g., Math, English, Science, etc.). You can enter your grades into our tool to calculate your cluster points automatically.",
+  },
+  {
+    question: "How are KUCCPS cluster points calculated?",
+    answer:
+      "KUCCPS cluster points are calculated using a formula that considers your high school grades in specific subjects related to the course you want to pursue. You can use our online tool to calculate your cluster points.",
+  },
+  {
+    question: "How can I know which courses I qualify for?",
+    answer:
+      "By entering your cluster points, you can see a list of degree, diploma, and certificate courses that you are eligible for, along with the universities that offer those courses.",
+  },
+  {
+    question:
+      "What is the difference between a degree, diploma, and certificate course?",
+    answer:
+      "A degree course typically lasts 3–5 years and is for students looking to earn a bachelor's degree. A diploma course is usually 2–3 years, and a certificate course is shorter, typically 1–2 years. Each qualification opens different career paths and opportunities.",
+  },
+  {
+    question: "How are KUCCPS cluster points calculated?",
+    answer:
+      "KUCCPS cluster points are calculated using a formula that considers your high school grades in specific subjects related to the course you want to pursue. You can use our online tool to calculate your cluster points.",
+  },
+  {
+    question:
+      "How can I improve my cluster points to qualify for a better course?",
+    answer:
+      " To improve your cluster points, focus on performing well in subjects relevant to the course you're interested in. You can also explore alternative pathways, such as diploma or certificate courses, which may have different requirements.",
+  },
+];
