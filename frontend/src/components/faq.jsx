eb81b61f914167eece783f43789844ba016ac496
mercy masaa <masaakavulanya@gmail.com>
2025-04-03 06:30:02 +0300

    create initial Team component with title and description

--- frontend/src/components/faq.jsx
+++ frontend/src/components/faq.jsx
@@ -12,7 +12,7 @@ const Faq = () => {
   };
 
   return (
-    <div className="max-w-3xl mx-auto p-8">
+    <div className="max-w-3xl mx-auto p-8 bg-gray-100">
       {/* heading */}
       <h1 className="text-2xl font-bold mb-6 text-gray-800">
         Frequently Asked Questions
--- frontend/src/components/teams.jsx
+++ frontend/src/components/teams.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Teams = () => {
   return (
-    <div>teams</div>
-  )
+    <section  className="py-16 bg-gray-100">
+    <div className="max-w-6xl mx-auto px-6">
+      <h1 className="text-3xl font-bold text-center text-gray-800">
+        Our <span className="text-indigo-600">Teams</span>
+      </h1>
+      <h3 className="text-lg text-gray-600 text-center mt-2">
+        Meet our diverse team of creative minds, developers, and strategists — 
+        the driving force behind every project's success.
+      </h3>
+    </div>
+  </section>
+);
+  
 }
 
 export default Teams
